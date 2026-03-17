d467f48d545aa13f739d500f3f65185ecc1d791b
drpizza <cole.colesr.sam@gmail.com>
2026-03-17 03:51:41 -0400

    converted to Python

--- GamblingGame_1.1.py
+++ GamblingGame_1.1.py
@@ -1,13 +1,7 @@
 # A simple high/low gambling game
 
-
-# imports the random package to use random function
 import random
 import sys
-from rich.progress import Progress
-import time
-
-from nbformat.v3.nbbase import cast_str
 
 myCash = int(1000)
 startingCash = myCash  # track starting amount for net summary
@@ -16,20 +10,20 @@ print("WELCOME TO SAMS HIGH/LOW GAMBLING")
 myName = input("What is your name? ").capitalize()
 # create "loading/status" bar
 print("Accessing profile for " + myName + "...")
-print("Cash available to wager: ", myCash, ".")
+print("Cash available to wager: ", myCash)
 
 while myCash > 0:
     print("****************************************************************************")
 
     # collects adn validates bet amount
-    myBet = int(input("How much money would you like to bet on this round? "))
-    if myBet > startingCash:
+    myBet = int(input("How much money would you like to bet this round? "))
+    if myBet > myCash:
         print("You do not have enough money to bet that amount. Please reduce bet amount.")
-        myBet = startingCash
+        myBet = myCash
     if myBet <= 0:
         print("Bet must be greater than $0. Please try again.")
         continue
-    print("You have a cash balance of", myCash, "and are betting", myBet, ".")
+    print("You have a cash balance of", myCash, "and are betting", myBet)
 
     # generates a random number
     firstNumber = random.randint(1, 7777777)
@@ -82,7 +76,7 @@ while myCash > 0:
     if addCash == "yes":
         addCashAmt = int(input("How much money would you like to add? $"))
         myCash += int(addCashAmt)
-        startingCash += addCashAmt  # adjust baseline so net stays accurate
+        #startingCash += addCashAmt  # adjust baseline so net stays accurate
         print("Your NEW CASH BALANCE is $", myCash)
 
     elif addCash == 'no':
